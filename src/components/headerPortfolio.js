a5f6cca825a8248b7952735a66418daabd60b632
Charles Lang <Charles.Lang@corp.canvas.org>
2019-07-23 09:37:30 -0600

    Header Portfolio Edits

--- src/components/headerPortfolio.js
+++ src/components/headerPortfolio.js
@@ -1,29 +1,45 @@
-import React, { Component } from 'react';
-import { Popover, OverlayTrigger} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Popover, PopoverBody } from "reactstrap";
 
-import '../styles/headerPortfolio.css';
+import "../styles/headerPortfolio.css";
 
 class HeaderPortfolio extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
-    const popover = (
-      <Popover id="popover-basic">
-        <Popover.Title as="h3">Popover right</Popover.Title>
-        <Popover.Content>
-          And here's some <strong>amazing</strong> content. It's very engaging.
-          right?
-        </Popover.Content>
-      </Popover>
-    );
-    
-    const Example = () => (
-      <OverlayTrigger trigger="click" placement="bottom" overlay={{popover}}>
-        <div className="profilePicture"></div>
-      </OverlayTrigger>
-    );
-    
     return (
-      <Example />
-    )
+      <div>
+        <div className="profilePicture" id="Popover1"/>
+
+        <Popover
+          placement="bottom"
+          isOpen={this.state.popoverOpen}
+          target="Popover1"
+          toggle={this.toggle}
+        >
+          <PopoverBody>
+            <div className="row">
+              <div className="col">
+                <p>Test</p>
+              </div>
+            </div>
+          </PopoverBody>
+        </Popover>
+      </div>
+    );
   }
 }
 
